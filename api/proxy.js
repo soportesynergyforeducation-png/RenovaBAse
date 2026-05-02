0fc5f55ea4ff8667ba8c99839c2286603040e5aa
Samuel Diaz - Soporte Técnico <soportesynergyforeducation@gmail.com>
2026-05-01 20:25:50 -0600

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,5 +1,9 @@
 const GAS_URL = 'https://script.google.com/macros/s/AKfycbxSrcAKPWAxX0GW73QTQOkfl2s6GusWk3XIYyKSl6L7Iq_4AfjxrprCNPanOwArG1yvIA/exec';
 
+export const config = {
+  maxDuration: 60,
+};
+
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
